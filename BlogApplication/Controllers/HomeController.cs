878674312d9f302ba0a2a5c66783b7afe2a1235b
Kato <kato.stolen@itverket.no>
2014-11-04 00:24:13 +0100

    Initial commit

---
+++ BlogApplication/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web.Mvc;
+using ApplicationModel;
+using ApplicationModel.Repositories;
+using BlogApplication.Models;
+
+namespace BlogApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AboutRepository _repository = new AboutRepository();
+
+        public ActionResult About()
+        {
+            var repository = new AboutRepository();
+
+            var aboutInformation = repository.GetAboutInformation();
+            var model = new AboutInformationViewModel();
+            if (aboutInformation != null)
+            {
+                model.AboutMe = aboutInformation.AboutMe;
+                model.BirthDate = aboutInformation.BirthDate;
+                model.FirstName = aboutInformation.FirstName;
+                model.LastName = aboutInformation.LastName;
+            }
+           
+            return View(model);
+        }
+    }
+}
